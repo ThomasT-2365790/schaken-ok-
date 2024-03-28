06fe69f5c60b10484c77ba98191c49fa0b1b5543
Arne Bex <arne.bex@student.uhasselt.be>
2024-03-28 08:52:17 +0100

    Tower bijna af alleen nog piece at doorlussen.

--- Project1/Tower.cpp
+++ Project1/Tower.cpp
@@ -2,13 +2,66 @@
 #include <iostream>
 
 bool Tower::is_pos(Cord _now, Cord _new) {
-	if (_new.getcolum() == _now.getcolum() || _now.getrow() == _new.getrow()) {
-		return true;
+	/*
+	if (_new.getcolum() == _now.getcolum()) {
+		return between_row();
+	}
+	if (_now.getrow() == _new.getrow()) {
+		return between_colum();
 	}
 	return false;
+	*/
+	if (_new.getcolum() == _now.getcolum() || _now.getrow() == _new.getrow()) { return true; }
+	return false;
 }
+/*
+bool Tower::between_row(Cord _now, Cord _new) {
+	int afstand = _new.getrow() - _now.getrow();
+	if (afstand == 0) { return false; }
+	if (afstand == -1 || afstand == 1) { return true; }
+	if (afstand < -1) {
+		while (afstand < -1) {
+			afstand += 1;
+			if (piece_at(Cord((_now.getrow() + afstand), _now.getcolum()) { return false; }
+		}
+		return true;
+	}
+	else {
+		while (afstand > 1) {
+			afstand -= 1;
+			if (piece_at(Cord((_now.getrow() + afstand), _now.getcolum()) { return false; }
+		}
+		return true;
+	}
+}
+bool Tower::between_colum(Cord _now, Cord _new) {
+	int afstand = _new.getcolum() - _now.getcolum();
+	if (afstand == 0) { return false; }
+	if (afstand == -1 || afstand == 1) { return true; }
+	if (afstand < -1) {
+		while (afstand < -1) {
+			afstand += 1;
+			if (piece_at(Cord (_new.getrow(),(_now.getcolum()+afstand)) { return false; }
+		}
+		return true;
+	}
+	else {
+		while (afstand > 1) {
+			afstand -= 1;
+			if (piece_at(Cord (_new.getrow(),(_now.getcolum()+afstand)) { return false; }
+		}
+		return true;
+	}
+}*/
+
 Tower::Tower(Color kleur, Cord coord) : Piece{ kleur, coord } {};
 
 char Tower::return_type()  {
 	return 'T';
 }
+/*
+bool Tower::func_bord(Bord& bord,Cord cord) {
+	if (bord.compare_cord(bord.piece_at(cord),Cord(0,0))) { return true; }
+	return false;
+}
+*/
--- Project1/Tower.h
+++ Project1/Tower.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Piece.h"
+#include "bord.h"
 
 class Tower : public Piece
 {
@@ -7,7 +8,10 @@ public:
 	Tower(Color color, Cord cord);
 	bool is_pos(Cord _now, Cord _new) override;
 	char return_type()  override;
-
+/*	bool between_row(Cord _now, Cord _new);
+	bool between_colum(Cord _now, Cord _new);
+	bool func_bord(Bord& bord,Cord cord);
+	*/
 private:
 
 };
